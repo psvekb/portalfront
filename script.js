5aad07e4efd0462bdadb1209d03d7f0fed5d834d
Sergej Pakhomov <psv.ekb@gmail.com>
2022-08-05 23:46:53 +0500

    calc minbatt quantity for time & power

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ import pnList from "./pnList.js";
 const UPS_EFFICIENCY = 0.94; // КПД ИБП
 const kilo = 1000;
 
-const [fullPower, pF, timeInMinutes, upsFamily] = [100, 0.8, 10, "E3M"]; //100 kVA, pF=0.8, 10 min
+const [fullPower, pF, requestedTimeInMinutes, upsFamily] = [
+  100,
+  0.8,
+  10,
+  "E3M",
+]; //100 kVA, pF=0.8, 10 min
 // console.log("input Array: ", inputArray);
 
 const outputArray = ["U3MUPS100KH", "BC1100", "HF12-135W-X"];
@@ -15,8 +20,8 @@ const pnListArray = pnListArrayStrings.map((string) => string.split(";"));
 
 const batteryInfoStartIndex = 41;
 const batteryInfoEndIndex = batteryInfoStartIndex + 21;
-const batteryPowerStartIndex = 9;
-const batteryPowerEndIndex = batteryPowerStartIndex + 5;
+const batteryPowerStartIndex = 8;
+const batteryPowerEndIndex = batteryPowerStartIndex + 6;
 
 const batteryPowertable = pnListArray
   .slice(batteryInfoStartIndex, batteryInfoEndIndex)
@@ -27,18 +32,33 @@ const batteryPowertable = pnListArray
 // let numberOfStrings = 1; //1..4
 let activePower = fullPower * pF;
 
-let [Rslope, Scal, Expo, Cap, Volt] = [0.094, 0.575, 1.213, 45.0, 12];
-
-for (let strings = 1; strings <= 4; strings++) {
-  for (let blockInString = 36; blockInString <= 50; blockInString += 2) {
-    let powerFromBlock =
-      (activePower / UPS_EFFICIENCY / blockInString / strings) * kilo; //
-    // console.log("powerFormBlock : ", powerFromBlock);
-    let time = (
-      60 *
-      Scal *
-      (((Cap * Volt) / powerFromBlock) ** Expo - Rslope)
-    ).toFixed(2);
-    console.log(`string x bat : ${strings}x${blockInString} == time ${time}`);
+const getMinBattery = () => {
+  for (let strings = 1; strings <= 4; strings++) {
+    for (let blockInString = 36; blockInString <= 50; blockInString += 2) {
+      let powerFromBlock =
+        (activePower / UPS_EFFICIENCY / blockInString / strings) * kilo; //
+      let time = //Peukert's law - формула Пекерта
+        (
+          60 *
+          Scal *
+          (((Cap * Volt) / powerFromBlock) ** Expo - Rslope)
+        ).toFixed(2);
+      if (time >= requestedTimeInMinutes) {
+        console.log(
+          `string x bat : ${strings}x${blockInString} == time ${time} price ${(
+            batteryPrice *
+            strings *
+            blockInString
+          ).toFixed(0)} USD`
+        );
+        return strings * blockInString;
+      }
+    }
   }
+};
+
+for (let i = 0; i < batteryPowertable.length; i++) {
+  var [batteryPrice, Rslope, Scal, Expo, Cap, Volt] = batteryPowertable[i];
+
+  console.log(getMinBattery());
 }
