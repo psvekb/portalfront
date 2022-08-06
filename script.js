13ee19ae1b6857ff4352c4299153f175047b6b0f
Sergej Pakhomov <psv.ekb@gmail.com>
2022-08-06 19:28:32 +0500

    make clear function for batt calc

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 import pnList from "./pnList.js";
 const UPS_EFFICIENCY = 0.94; // КПД ИБП
-const kilo = 1000;
+const KILO = 1000;
 
-const [fullPower, pF, requestedTimeInMinutes, upsFamily] = [
-  100,
-  0.8,
-  10,
-  "E3M",
-]; //100 kVA, pF=0.8, 10 min
+// get from API in future
+const {
+  upsFamily,
+  outputPower,
+  LoadpF,
+  redundancy,
+  actualLoadPercentage,
+  batteryRuntime,
+  batteryTypeAndPlacement,
+  ingressProtection,
+} = {
+  upsFamily: "E3M",
+  outputPower: 100, //kVA
+  LoadpF: 0.8,
+  redundancy: "N",
+  actualLoadPercentage: 100, // %
+  batteryRuntime: 10, //min
+  batteryTypeAndPlacement: "racks",
+  ingressProtection: "IP20",
+}; //100 kVA, LoadpF=0.8, 10 min
 // console.log("input Array: ", inputArray);
 
 const outputArray = ["U3MUPS100KH", "BC1100", "HF12-135W-X"];
@@ -26,24 +40,23 @@ const batteryPowerEndIndex = batteryPowerStartIndex + 6;
 const batteryPowertable = pnListArray
   .slice(batteryInfoStartIndex, batteryInfoEndIndex)
   .map((row) => row.slice(batteryPowerStartIndex, batteryPowerEndIndex));
-// console.log("batteryPowertable: ", batteryPowertable);
 
-// let blockInString = 38; //36 ... 50
-// let numberOfStrings = 1; //1..4
-let activePower = fullPower * pF;
+let powerFromBattery = outputPower * LoadpF * (actualLoadPercentage / 100);
 
-const getMinBattery = () => {
+const getMinimumBatteryLength = (batteryPower) => {
+  let [batteryPrice, Rslope, Scal, Expo, Cap, Volt] = batteryPower;
+
   for (let strings = 1; strings <= 4; strings++) {
     for (let blockInString = 36; blockInString <= 50; blockInString += 2) {
       let powerFromBlock =
-        (activePower / UPS_EFFICIENCY / blockInString / strings) * kilo; //
+        (powerFromBattery / UPS_EFFICIENCY / blockInString / strings) * KILO; //
       let time = //Peukert's law - формула Пекерта
         (
           60 *
           Scal *
           (((Cap * Volt) / powerFromBlock) ** Expo - Rslope)
         ).toFixed(2);
-      if (time >= requestedTimeInMinutes) {
+      if (time >= batteryRuntime) {
         console.log(
           `string x bat : ${strings}x${blockInString} == time ${time} price ${(
             batteryPrice *
@@ -51,14 +64,25 @@ const getMinBattery = () => {
             blockInString
           ).toFixed(0)} USD`
         );
-        return strings * blockInString;
+        return [strings, blockInString, batteryPrice * strings * blockInString];
       }
     }
   }
+  return false;
 };
 
-for (let i = 0; i < batteryPowertable.length; i++) {
-  var [batteryPrice, Rslope, Scal, Expo, Cap, Volt] = batteryPowertable[i];
+const getBatteryVariantsTable = (batteryPowertable) => {
+  let batteryCalculatedLengthPriceTable = [];
+  for (let i = 0; i < batteryPowertable.length; i++) {
+    let batteryPower = batteryPowertable[i];
+    let search = getMinimumBatteryLength(batteryPower);
+    if (search) {
+      batteryCalculatedLengthPriceTable.push([i, ...search]);
+    }
+  }
+  batteryCalculatedLengthPriceTable.sort((a, b) => a[3] - b[3]);
 
-  console.log(getMinBattery());
-}
+  return batteryCalculatedLengthPriceTable;
+};
+
+console.log(getBatteryVariantsTable(batteryPowertable));
